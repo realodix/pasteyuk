c3f1e421561ac31c159fa6cc9db61ce37f0573bb
Kei <realodix@yahoo.com>
2018-08-04 13:19:22 +0700

    rev-180804

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+// use App\Http\Requests;
 use App\Paste;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
