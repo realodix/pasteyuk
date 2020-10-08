c4e6903d809b032f1083b02f832f2eff7fbb1ba3
realodix <budi03@gmail.com>
2020-10-08 18:26:49 +0700

    laravel-mix v5.0.1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,36 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
-mix.options({
- processCssUrls: false
-});
+mix
+  .sass('resources/sass/pasteyuk.scss', 'public/css')
+  .sass('resources/sass/auth/auth.scss', 'public/css')
+  .sass('resources/sass/bootstrap-purple/bootstrap-purple.scss', 'public/css')
+  .js([
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+  ], 'public/js/app.js')
+  .copyDirectory('node_modules/jquery/dist', 'public/vendor/jquery')
+  .copyDirectory('node_modules/@fortawesome/fontawesome-free', 'public/vendor/fontawesome');
 
-mix.sass('resources/sass/pasteyuk.scss', 'public/css', {outputStyle: 'expanded'})
-   .sass('resources/sass/auth/auth.scss', 'public/css')
-   .sass('resources/sass/bootstrap-purple/bootstrap-purple.scss', 'public/css', {outputStyle: 'expanded'})
-   .js([
-      'node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-   ], 'public/js/app.js');;
+mix
+  .extract()
+  .version()
+  .setPublicPath('public')
+  .options({
+      autoprefixer: false,
+      processCssUrls: false,
+  });
 
-mix.copyDirectory('node_modules/jquery/dist', 'public/vendor/jquery');
-mix.copyDirectory('node_modules/@fortawesome/fontawesome-free', 'public/vendor/fontawesome');
+if (!mix.inProduction()) {
+    mix
+      .webpackConfig({
+          devtool: 'source-map',
+      })
+      .sourceMaps()
+      .browserSync({
+          open: 'external',
+        host: 'urlhub.test',
+          proxy: 'urlhub.test'
+      })
+}
 
 // mix.js('resources/assets/js/app.js', 'public/js')
 //    .sass('resources/assets/sass/app.scss', 'public/css');
